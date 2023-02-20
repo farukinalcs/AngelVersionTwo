cac0e46b29f8fdb19642b051126fc0f3430c6903
farukinalcs <farukinalcs@gmail.com>
2023-02-20 09:51:06 +0300

    shared module

---
+++ src/app/_angel/shared/sicil-liste/sicil-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sicil-liste',
+  templateUrl: './sicil-liste.component.html',
+  styleUrls: ['./sicil-liste.component.scss']
+})
+export class SicilListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
