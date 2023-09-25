b667cab365b71dd34c1090a2321d77b392121021
farukinalcs <farukinalcs@gmail.com>
2023-09-25 08:51:18 +0300

    25.09

---
+++ src/app/_angel/profile/profil-tanimlamalar/yemek-tipi-tanimlama/yemek-tipi-tanimlama.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yemek-tipi-tanimlama',
+  templateUrl: './yemek-tipi-tanimlama.component.html',
+  styleUrls: ['./yemek-tipi-tanimlama.component.scss']
+})
+export class YemekTipiTanimlamaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
